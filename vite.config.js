726bfea6b2c28e62ca2efebf412080278644d631
si-iguana <cimoncimonjr@gmail.com>
2025-08-26 09:49:01 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/labkes-website/",
   plugins: [react()],
+  base: '/labkesdakabsukabumi.dpdns.org/', // ganti dengan nama repo GitHub kamu
 })
