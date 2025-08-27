963d89b07ed53f6f023d0a08b5c73fdea23ea9d6
si-iguana <cimoncimonjr@gmail.com>
2025-08-27 09:22:02 +0700

    deploy react app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", // pastikan ke folder src
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
