ebe037b76d169de71a715e9735cacc6437de8def
si-iguana <cimoncimonjr@gmail.com>
2025-08-27 09:42:35 +0700

    Save changes before deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./app";
 import "./index.css"; // opsional kalau ada styling
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
